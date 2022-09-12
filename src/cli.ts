8b6e8cb00d038e4775f308120e302ada57d14630
Andrey S <pesopi@mail.ru>
2022-09-12 16:47:41 +0300

    Добавляет команду --generate

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
 import CLIApplication from './app/cli-application.js';
+import GenerateCommand from './cli-command/generate-command.js';
 import HelpCommand from './cli-command/help-command.js';
 import ImportCommand from './cli-command/import-command.js';
 import VersionCommand from './cli-command/version-command.js';
 
 const myManager = new CLIApplication();
 myManager.registerCommands([
-  new HelpCommand, new VersionCommand, new ImportCommand
+  new HelpCommand, new VersionCommand, new ImportCommand, new GenerateCommand
 ]);
 myManager.processCommand(process.argv);
