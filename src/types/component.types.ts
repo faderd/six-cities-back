0c3af0936c55cbb5911346bec3aa47c01a614e69
Andrey S <pesopi@mail.ru>
2022-09-18 15:07:01 +0300

    Подготавливает инфраструктуру для разработки REST API сервиса

---
+++ src/types/component.types.ts
@@ -0,0 +1,5 @@
+export const Component = {
+  Application: Symbol.for('Application'),
+  LoggerInterface: Symbol.for('LoggerInerface'),
+  ConfigInterface: Symbol.for('ConfigInerface'),
+} as const;
